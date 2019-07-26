f882dfed8f77a6a2b5d67b2c7b327ea60aba43b3
Nia Bert <nbert14@gmail.com>
2019-07-26 01:58:59 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -3,7 +3,7 @@ INSERT INTO users (id, name, age) VALUES (2, 'Bear', 6);
 INSERT INTO users (id, name, age) VALUES (3, 'Iguana', 4)
 INSERT INTO users (id, name, age) VALUES (4, 'Alex', 33)
 INSERT INTO users (id, name, age) VALUES (5, 'Amanda', 24))
-INSERT INTO users (id, name, age) VALUES (1, 'Finnebar', 17)
+INSERT INTO users (id, name, age) VALUES (6, 'Sophie', 24)
 INSERT INTO users (id, name, age) VALUES (1, 'Finnebar', 17)
 INSERT INTO users (id, name, age) VALUES (1, 'Finnebar', 17)
 INSERT INTO users (id, name, age) VALUES (1, 'Finnebar', 17)
